fc6dd8e6780bbbcfa7dc30d478f40032fb9f906d
Nelson Adagi <thinkiqbs@gmail.com>
2025-02-17 13:35:44 +0300

    Update changes

--- s3/s3/settings.py
+++ s3/s3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-8)=gs7mo02$quj5%o91d*)fsh#*55b8&7$n7ick$rxoa1ufy*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
