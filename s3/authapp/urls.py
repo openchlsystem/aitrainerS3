826f4819224404906f4f4ed36265a321f4cf656e
Franklin Karanja <franklinkaranja774@gmail.com>
2025-02-19 00:50:15 +0300

    refresh_url fix

--- s3/authapp/urls.py
+++ s3/authapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path("request-otp/", RequestOTPView.as_view(), name="request-otp"),
     path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("refresh-token/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 
