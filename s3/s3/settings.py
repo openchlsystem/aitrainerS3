45059f142302b43f4f529ba29ffdc606ad031a12
Franklin Karanja <franklinkaranja774@gmail.com>
2025-02-19 10:39:36 +0300

    updated mysql settings

--- s3/s3/settings.py
+++ s3/s3/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +90,24 @@ WSGI_APPLICATION = "s3.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "aitraining_data",
+        "USER":"root",
+        "HOST":"localhost",
+        "PASSWORD":"2025Ai@2025!",
+        "PORT":"",
+    #    'OPTIONS': {
+    #         'unix_socket': '/var/lib/mysql/mysql.sock',  
+    #     },
     }
 }
 
