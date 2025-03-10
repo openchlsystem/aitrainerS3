e667babb5b864c5861c009859c65e7c35b732fb7
Franklin Karanja <franklinkaranja774@gmail.com>
2025-03-10 16:12:27 +0300

    server prod settings

--- s3/s3/settings/production.py
+++ s3/s3/settings/production.py
@@ -2,21 +2,24 @@ from .base import *
 
 # Production settings
 DEBUG = False
-ALLOWED_HOSTS = ['your-production-domain.com']
+ALLOWED_HOSTS = ['edms-enpoints.bitz-itc.com']
 
 # Production Database Configuration
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "production_db_name",
-        "USER": "production_db_user",
-        "PASSWORD": "production_db_password",
-        "HOST": "production_db_host",
-        "PORT": "",
+        "NAME": "aitraining_data",
+        "USER":"root",
+        "HOST":"localhost",
+        "PASSWORD":"2025Ai@2025!",
+        "PORT":"",
+    #    'OPTIONS': {
+    #         'unix_socket': '/var/lib/mysql/mysql.sock',  
+    #     },
     }
 }
 
 # Stricter security settings
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
